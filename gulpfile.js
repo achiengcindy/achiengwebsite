af36fbc3506238cbc0f07b80d562d548ae1f8eb1
Cindy Muga <achiengcindy36@gmail.com>
2019-05-07 11:16:26 +0300

    dependencies update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
+let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let pipeline = require('readable-stream').pipeline;
+let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('uglify', function(){
+gulp.task('minify-js', () => {
 	  return pipeline(
-        gulp.src('./src/static/js/**/*.js'),
+        gulp.src('./src/static/dev/js/**/*.js'),
         uglify(),
-        gulp.dest('./src/static/dist/js/')
+        gulp.dest('./src/static/js/')
   );
 
 });
+
+
+gulp.task('minify-css', () => {
+	return pipeline(
+		gulp.src('./src/static/dev/css/**/*.css'),
+        cleanCSS({compatibility: 'ie8'}),
+        gulp.dest('./src/static/css/')
+
+
+		);
+});
+
+
+gulp.task('resize-image', () => {
+	return pipeline(
+		gulp.src('./src/static/dev/images/**/*.css'),
+        imagemin(),
+        gulp.dest('./src/static/images/')
+
+
+		);
+});
+
  
- gulp.task("minify", gulp.series('uglify'));
+gulp.task("minify", gulp.series('minify-js', 'minify-css', 'resize-image'));
 
 
+
+
+
+
+ 
